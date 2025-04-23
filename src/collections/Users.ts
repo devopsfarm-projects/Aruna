26289f49d879c9dc46784c131795fe3e33d403b4
Trilok <tiosuthar@gmail.com>
2025-04-23 11:11:46 +0530

    all update

---
+++ src/collections/Users.ts
@@ -0,0 +1,58 @@
+import type { CollectionConfig } from 'payload';
+
+export const Users: CollectionConfig = {
+  slug: 'users',
+  admin: {
+    useAsTitle: 'email',
+  },
+  auth: true,
+  access: {
+    create: ({ req: { user } }) => user?.role === 'owner',
+
+    delete: ({ req: { user } }) => user?.role === 'owner',
+
+    read: ({ req: { user } }) => {
+      if (!user) return false;
+      return user.role === 'owner' || {
+        id: {
+          equals: user.id,
+        },
+      };
+    },
+
+    update: ({ req: { user } }) => {
+      if (!user) return false;
+      return user.role === 'owner' || {
+        id: {
+          equals: user.id,
+        },
+      };
+    },
+  },
+
+  fields: [
+    {
+      name: 'role',
+      type: 'select',
+      required: true,
+      defaultValue: 'client',
+      options: [
+        {
+          label: 'Owner',
+          value: 'owner',
+        },
+        {
+          label: 'Client',
+          value: 'client',
+        },
+        {
+          label: 'Sites Visitor',
+          value: 'sites-visitor',
+        },
+      ],
+      access: {
+        update: ({ req: { user } }) => user?.role === 'owner',
+      },
+    },
+  ],
+};
