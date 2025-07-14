e1f96498c322209997e7a17dd38cefa85c273017
YAMRAAJ0 <tiosuthar@gmail.com>
2025-07-14 14:44:46 +0530

    code update

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,10 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import { env } from '@/env.mjs';
+// import { drizzle } from 'drizzle-orm/postgres-js';
+// import { env } from '@/env.mjs';
 
-if (!env.DATABASE_URL) {
-  throw new Error('DATABASE_URL environment variable is required');
-}
+// if (!env.DATABASE_URL) {
+//   throw new Error('DATABASE_URL environment variable is required');
+// }
 
-const queryClient = drizzle(env.DATABASE_URL as string);
+// const queryClient = drizzle(env.DATABASE_URL as string);
 
-export const db = queryClient;
+// export const db = queryClient;
